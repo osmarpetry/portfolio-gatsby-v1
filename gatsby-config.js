407b691a8a05d3306544c954a10bee40c6db59d1
osmarpetry <osmar.petry-ext@rio.cloud>
2020-08-04 21:10:40 -0300

    google tag is implemented

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,13 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
     {
+      resolve: "gatsby-plugin-google-tagmanager",
+      options: {
+        id: "GTM-KQ64DC7",
+        includeInDevelopment: false,
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `uploads`,
