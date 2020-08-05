735303e40b0ff81d4185efcc85a872e205a3e36d
osmarpetry <osmar.petry-ext@rio.cloud>
2020-08-04 22:11:38 -0300

    not found layout

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,14 +1,50 @@
 import React from 'react'
+import { Link, graphql, useStaticQuery } from 'gatsby'
+import Img from 'gatsby-image'
+import styled from 'styled-components'
 
 import Layout from '../components/Layout'
 import SEO from '../components/seo'
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+const NotFoundLayout = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+const SadImage = styled(Img)`
+  border-radius: 5px;
+  margin-bottom: 10px;
+`
+
+const NotFoundPage = () => {
+  const { sadImage } = useStaticQuery(
+    graphql`
+      query {
+        sadImage: file(relativePath: { eq: "404.jpg" }) {
+          childImageSharp {
+            fixed(width: 400, height: 400) {
+              ...GatsbyImageSharpFixed_tracedSVG
+            }
+          }
+        }
+      }
+    `
+  )
+
+  return (
+    <Layout>
+      <SEO title="Page not found" />
+      <NotFoundLayout>
+        <h1>Page NOT FOUND!</h1>
+        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        <SadImage fixed={sadImage.childImageSharp.fixed} />
+        <p>
+          <Link to="/">Click here to return to home page</Link>
+        </p>
+      </NotFoundLayout>
+    </Layout>
+  )
+}
 
 export default NotFoundPage
