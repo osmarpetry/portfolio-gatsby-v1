dcd468e5f2c7f061b53b3de08a30abf074dc1684
osmarpetry <osmar.petry-ext@rio.cloud>
2020-08-08 00:11:52 -0300

    sentry configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,6 +17,15 @@ module.exports = {
       },
     },
     {
+      resolve: "gatsby-plugin-sentry",
+      options: {
+        dsn: "https://631e567bf17443d59ad4358880352360@o431471.ingest.sentry.io/5382606",
+        // Optional settings, see https://docs.sentry.io/clients/node/config/#optional-settings
+        environment: process.env.NODE_ENV,
+        enabled: (() => ["production", "stage"].indexOf(process.env.NODE_ENV) !== -1)()
+      }
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `uploads`,
